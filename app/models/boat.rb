54195af3dc31161c1833aa377a7159038c98658c
Kgallagher281 <kevin.gallagher23@gmail.com>
2017-11-01 08:29:56 -0700

    initial paperclip

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,10 +1,9 @@
-class Boat < ApplicationRecord
-  belongs_to :user
-  has_one :job
-  has_many :containers
+class Boat < ApplicationRecord   
+belongs_to :user   
+has_one :job   
 
-  def containers
-  end
-  
 
+has_attached_file :photo, styles: { medium: "200x200>", thumb: "50x50>" }, default_url: "/missing.png"
+	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
+
